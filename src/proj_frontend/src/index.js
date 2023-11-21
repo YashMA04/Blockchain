f9d006eba58e508912d24acb538ee4e89db2a7f4
YashMA04 <149203603+YashMA04@users.noreply.github.com>
2023-11-21 11:05:00 +0000

    changes

---
+++ src/proj_frontend/src/index.js
@@ -0,0 +1,19 @@
+// import { proj_backend } from "../../declarations/proj_backend";
+
+// document.querySelector("form").addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   const button = e.target.querySelector("button");
+
+//   const name = document.getElementById("name").value.toString();
+
+//   button.setAttribute("disabled", true);
+
+//   // Interact with foo actor, calling the greet method
+//   const greeting = await proj_backend.greet(name);
+
+//   button.removeAttribute("disabled");
+
+//   document.getElementById("greeting").innerText = greeting;
+
+//   return false;
+// });
